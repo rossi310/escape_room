9cb2f397010f168ab6ccfe16fa1dc0bb2b075959
rossi310 <139461320+rossi310@users.noreply.github.com>
2025-06-30 17:06:18 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,10 +18,10 @@ def display_pdf2(file_path):
 
 # Define codes and corresponding PDF files for each escape room
 escape_room_1 = {
-    "3": "pdfs/ESCAPE ROOM 1-Puzzle 2.JPG",
-    "1136": "pdfs/ESCAPE ROOM 1-Puzzle 3.JPG",
-    "1869": "pdfs/ESCAPE ROOM 1-Puzzle 4.JPG",
-    "4135": "pdfs/ESCAPE ROOM 1-Puzzle 5.JPG"
+    "3": "pdfs/ESCAPE ROOM 1-Puzzle 2.jpg",
+    "1136": "/pdfs/ESCAPE ROOM 1-Puzzle 3.jpg",
+    "1869": "pdfs/ESCAPE ROOM 1-Puzzle 4.jpg",
+    "4135": "pdfs/ESCAPE ROOM 1-Puzzle 5.jpg"
 }
 
 escape_room_2 = {
